31a32223d7ecc868e8d317ab549bc1f1901b7dd3
Abel Van bergen en henegouwen <avan-ber@f0r5s3.codam.nl>
2021-05-12 18:44:01 +0200

    t begin

---
+++ philo_one/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: avan-ber <avan-ber@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/05/12 18:14:59 by avan-ber      #+#    #+#                 */
+/*   Updated: 2021/05/12 18:36:28 by avan-ber      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_one.h"
+
+int main(int ac, char **av)
+{
+	if (ac == 5 && ac == 6)
+	{
+		
+	}
+	else
+		printf("Error: not the right amount of arguments\n");
+}
---
+++ philo_one/philo_one.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   philo_one.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: avan-ber <avan-ber@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/05/12 18:19:29 by avan-ber      #+#    #+#                 */
+/*   Updated: 2021/05/12 18:38:27 by avan-ber      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_ONE_H
+# define PHILO_ONE_H
+
+# include <stdio.h>
+
+/*
+** utils
+*/
+
+unsigned int    atoui(char *str);
+int             is_number(char* str);
+
+#endif
